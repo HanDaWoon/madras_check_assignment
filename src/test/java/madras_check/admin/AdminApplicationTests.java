87d28fab156ca97fa31298819e9cb034cfc59744
HanDaWoon <bookpia.han@gmail.com>
2024-06-23 14:10:35 +0900

    Initial Project

---
+++ src/test/java/madras_check/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package madras_check.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
